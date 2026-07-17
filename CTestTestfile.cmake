4bce56939dfe538d30b1798aad2b941c592bc036
Will <mrwge2010@gmail.com>
2026-07-17 23:00:21 +1000

    Delete CTestTestfile.cmake

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/mrwge/Documents/anti gravity/claude/osgui
-# Build directory: C:/Users/mrwge/Documents/anti gravity/claude/osgui/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[osgui_core]=] "C:/Users/mrwge/Documents/anti gravity/claude/osgui/build/osgui_tests.exe")
-set_tests_properties([=[osgui_core]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/mrwge/Documents/anti gravity/claude/osgui/CMakeLists.txt;81;add_test;C:/Users/mrwge/Documents/anti gravity/claude/osgui/CMakeLists.txt;0;")
